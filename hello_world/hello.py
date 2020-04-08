e575e98a07d0114842eaf7a2b2e482151723530d
Lasky206 <rick.allen206@gmail.com>
2020-04-07 23:37:52 -0600

    Re-organized files

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -2,9 +2,9 @@ from nameko.rpc import rpc
 from time import sleep
 
 class GreetingService:
-    name = "greeting_service"
+    name = 'greeting_service'
 
     @rpc
     def hello(self, name):
         sleep(5)
-        return "Hello, {}! (version 2)".format(name)
+        return 'Hello, {}! (version 2)'.format(name)
